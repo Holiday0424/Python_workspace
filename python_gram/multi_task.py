1b074ab4d144ed046649496cfff548243f72d310
Holiday_0424 <574048868@qq.com>
2025-11-14 09:20:07 +0800

    多线程demo

--- python_gram/multi_task.py
+++ python_gram/multi_task.py
@@ -7,10 +7,21 @@ def fir_task():
     temp_num = 0
     while temp_num < 10:
         print("这是任务1的输出")
+        time.sleep(1)
+        temp_num += 1
 
 def second_task():
     temp_num = 0
-    while temp_num < 3:
+    while temp_num < 5:
         print("这是任务2的输出")
         time.sleep(1)
         temp_num += 1
+
+def my_print():
+    fir_task_thread = threading.Thread(target = fir_task)
+    sec_task_thread = threading.Thread(target = second_task)
+    fir_task_thread.start()
+    sec_task_thread.start()
+
+    my_print()
+
