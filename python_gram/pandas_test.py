bf205cb523dc760a904319aee4e6d281366d632f
Holiday_0424 <574048868@qq.com>
2025-11-14 11:30:16 +0800

    pandas测试新demo

--- python_gram/pandas_test.py
+++ python_gram/pandas_test.py
@@ -1,14 +1,5 @@
-import numpy as np #pandas也需要与numpy相结合
 import pandas as pd
-print(f"np.nan的值为:\n{np.nan}\n----------")
 
-#Series创建
-s = pd.Series([1, 3, 5, np.nan, 6, 8])
-print(f"Series对象s的值:\n{s}")
-
-
-
-print('{0} {1}'.format('Hello', 'Python'))
-
-table = {'Sjoerd': 123, 'Jack': 456, 'Dcab': 789}
-for name, phone in table.items():print('{0:10} ==> {1:10d}'.format(name, phone))
+#Series对象
+series = pd.Series([1,2,3,4,5],name ='A')
+print(series)
