51f0426ce3500b10d5a93b513ce3c1bdaff98648
Holiday_0424 <574048868@qq.com>
2025-11-14 09:12:50 +0800

    111

--- python_gram/multi_task.py
+++ python_gram/multi_task.py
@@ -6,4 +6,11 @@ import threading,time
 def fir_task():
     temp_num = 0
     while temp_num < 10:
-        print("这是")
+        print("这是任务1的输出")
+
+def second_task():
+    temp_num = 0
+    while temp_num < 3:
+        print("这是任务2的输出")
+        time.sleep(1)
+        temp_num += 1
