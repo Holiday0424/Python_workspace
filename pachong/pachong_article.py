9b2c66e4857581c375a18ddef9bce5d8947de05a
Holiday_0424 <574048868@qq.com>
2026-01-06 11:44:05 +0800

    20260106-新增当当网爬虫示例

--- pachong/pachong_article.py
+++ pachong/pachong_article.py
@@ -1,6 +1,51 @@
 import requests
+from lxml import etree
+import csv
+
+url = 'http://search.dangdang.com/?key=Python&act=input'
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Referer': 'http://search.dangdang.com/',
+}
+
+
+def safe_get(lst, default=''):
+    """安全获取列表第一个元素"""
+    return lst[0].strip() if lst else default
+
+
+def parse_html(html):
+    selector = etree.HTML(html)
+    book_list = selector.xpath('//ul[@class="bigimg"]/li')  # 更健壮的选择方式
+    for book in book_list:
+        title = safe_get(book.xpath('.//a/@title'))
+        link = safe_get(book.xpath('.//a/@href'))
+        price = safe_get(book.xpath('.//span[@class="search_now_price"]/text()'))
+        author = safe_get(book.xpath('.//p[@class="search_book_author"]/span[1]/a/@title'))
+        publish_date = safe_get(book.xpath('.//p[@class="search_book_author"]/span[2]/text()')).replace('/', '').strip()
+        publisher = safe_get(book.xpath('.//p[@class="search_book_author"]/span[3]/a/@title'))
+
+        yield {
+            '书名': title,
+            '链接': link,
+            '价格': price,
+            '作者': author,
+            '出版日期': publish_date,
+            '出版社': publisher
+        }
+
+
+def save_data():
+    with open('dangdang_books.csv', 'w', newline='', encoding='utf-8-sig') as f:
+        writer = csv.DictWriter(f, fieldnames=['书名', '链接', '价格', '作者', '出版日期', '出版社'])
+        writer.writeheader()
+        response = requests.get(url, headers=headers)
+        response.encoding = 'gbk'  # 当当网常用 gbk 编码！
+        for item in parse_html(response.text):
+            if item['书名']:  # 只保存有效数据
+                writer.writerow(item)
+
 
 if __name__ == '__main__':
-    target = 'http://www.biqukan.com/1_1094/5403177.html'
-    req = requests.get(url=target)
-    print(req.text)
+    save_data()
