3a638948d71118acfb1d63ca39a3839336154abd
Holiday_0424 <574048868@qq.com>
2026-03-26 16:50:57 +0800

    20260326提交代码

---
+++ 1-数据类型/Datatype.py
@@ -0,0 +1,26 @@
+#Python中常用的有6种数据类型
+"""
+1、数字类型  [(整型 int)、浮点数(float)、复数(complex)、布尔(bool)]
+#整数：如10/-10;
+#浮点数：如13.14/-13.14
+#复数：如4+3j,以j结尾表示复数
+#布尔：true(1)/false(0)
+"""
+
+"""
+功能说明：
+1. 接收用户输入
+2. 验证数据有效性
+3. 返回处理结果
+"""
+
+
+string_type = '小金要学python'
+int_type = 10
+float_type = 10.1
+complex_type = 4+3j
+
+print(type(string_type))
+print(type(int_type))
+print(type(float_type))
+print(type(complex_type))
